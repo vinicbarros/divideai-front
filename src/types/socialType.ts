a5f84963667c24253094c79af36795fb154c6d45
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-02-03 09:19:08 -0300

    feat: new bill types

--- src/types/socialType.ts
+++ src/types/socialType.ts
@@ -5,24 +5,6 @@ export type FriendList = {
   email: string;
 };
 
-export type SendedFriendRequest = {
-  users_friendship_friendIdTousers: {
-    id: number;
-    name: string;
-    email: string;
-  };
-  id: number;
-};
-
-export type FriendRequest = {
-  users_friendship_userIdTousers: {
-    id: number;
-    name: string;
-    email: string;
-  };
-  id: number;
-};
-
 export interface IFriendResponse {
   friendRequestId: number;
   requestStatus: string;
